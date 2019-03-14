61e8856bb96381eeeda7478691391557a2169390
Russ Perry <rperry1886@gmail.com>
2019-03-14 11:48:50 -0400

    Add feature that shows the CSS code for you to copy and paste the linear gradient into your own code

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let css = document.querySelector("h3");
-let color1 = document.querySelector("#color1");
-let color2 = document.querySelector("#color2");
-let body = document.getElementById("gradient");
+let css = document.querySelector('h3');
+let color1 = document.querySelector('#color1');
+let color2 = document.querySelector('#color2');
+let body = document.getElementById('gradient');
 
 function setGradient() {
   body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
+  css.textContent = body.style.background + ';';
 }
 
 setGradient();
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
+color1.addEventListener('input', setGradient);
+color2.addEventListener('input', setGradient);
